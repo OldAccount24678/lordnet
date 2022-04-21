a20b3d825a6b55a4775b965b944294ab42ba2f9b
AmokDev <86306665+AmokDEV@users.noreply.github.com>
2022-04-21 12:48:22 +0300

    Create russia.py
    я гей

---
+++ modules/russia.py
@@ -0,0 +1,25 @@
+from helper import module, Message, db
+
+from pyrogram import filters
+
+#¯\_(ツ)_/¯
+
+@module(commands="russia", args=["on/off"], desc="заменяет буквы З и В на Z & V в тексте")
+async def example(_, message: Message):
+	try:
+		ZV = message.command[1]
+		db.set(f"Z", ZV)
+		await message.edit("[<b>RUSSIA</b>] Успешно!")
+	except IndexError:
+		await message.edit("[<b>RUSSIA</b>] Введите значения on/off!")
+
+@module(filters.me)
+async def russia(_, message: Message):
+	val = db.get("Z")
+	if val == "on":
+		text = message.text.replace("з", "Z").replace("в", "V").replace("З", "Z").replace("В", "V")
+		await message.edit(text)
+	else:
+		pass
+
+made_by = "@lordnet_modules | @AmokDev"

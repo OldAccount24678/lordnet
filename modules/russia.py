4fdb1b853ab6354c7ab770c96a62671b6390fa2d
AmokDev <86306665+AmokDEV@users.noreply.github.com>
2022-04-23 20:36:13 +0300

    Update russia.py
    амк сгрл

--- modules/russia.py
+++ modules/russia.py
@@ -3,18 +3,16 @@ from helper import module, Message, db
 from pyrogram import filters, ContinuePropagation
 
 #¯\_(ツ)_/¯
+# piton for loxs
 
 @module(commands="russia", args=["on/off"], desc="заменяет буквы З и В на Z & V в тексте")
 async def example(_, message: Message):
 	try:
 		ZV = message.command[1]
-		if ZV != "on" or ZV != "off":
-			await message.edit("[<b>RUSSIA</b>] Введите значения on/off!")
-		else:
-			db.set(f"Z", ZV)
-			await message.edit(f"[<b>RUSSIA</b>] Успешно поставлено значение {ZV}")
+		db.set(f"Z", ZV)
+		await message.edit(f"[<b>RUSSIA</b>] Успешно поставлено значение {ZV}")
 	except IndexError:
-		await message.edit("[<b>RUSSIA</b>] Введите значения on/off!")
+		await message.edit("[<b>RUSSIA</b>] Введите значения on/off! (IndexError)")
 
 @module(filters.me)
 async def russia(_, message: Message):

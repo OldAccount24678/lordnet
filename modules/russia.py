e1acc23f3e38eb4cb7aa6cd3a1aa48de67e075a6
AmokDev <86306665+AmokDEV@users.noreply.github.com>
2022-04-23 20:04:52 +0300

    Update russia.py
    песок 🙊

--- modules/russia.py
+++ modules/russia.py
@@ -1,11 +1,11 @@
 from helper import module, Message, db
 
-from pyrogram import filters
+from pyrogram import filters, ContinuePropagation
 
 #¯\_(ツ)_/¯
 
 @module(commands="russia", args=["on/off"], desc="заменяет буквы З и В на Z & V в тексте")
-async def russia(_, message: Message):
+async def example(_, message: Message):
 	try:
 		ZV = message.command[1]
 		db.set(f"Z", ZV)
@@ -14,12 +14,13 @@ async def russia(_, message: Message):
 		await message.edit("[<b>RUSSIA</b>] Введите значения on/off!")
 
 @module(filters.me)
-async def russiaZ(_, message: Message):
+async def russia(_, message: Message):
 	val = db.get("Z")
 	if val == "on":
 		text = message.text.replace("з", "Z").replace("в", "V").replace("З", "Z").replace("В", "V")
 		await message.edit(text)
 	else:
 		pass
+	raise ContinuePropagation
 
 made_by = "@lordnet_modules | @AmokDev"
